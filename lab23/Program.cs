4580226392508184a370c65b8b03ba692d29fec0
sergey leonov <redfortten@hotmail.com>
2021-11-01 23:05:24 +0300

    Добавьте файлы проекта.

---
+++ lab23/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab23
+{
+    //Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.
+    class Program
+    {
+        static int n;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Начало работы метода Main");            
+            try
+            {
+                Console.Write("Введите целое число, факториал которого хотите получить: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n >= 0)
+                {
+                    FactorialAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Число не должно быть отрицательным");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Console.WriteLine("Окончание работы метода Main");            
+            Console.ReadKey();
+        }
+        static void Factorial()
+        {
+            ulong result = 1;
+            if (n >= 2)
+            {
+                for (int i = 1; i <= (n - 1); i++)
+                {
+                    result *= (ulong)(i + 1);
+                }
+            }
+            Console.WriteLine($"{n}! = {result}");
+        }
+        static async void FactorialAsync()
+        {
+            //Console.WriteLine("Начало работы метода FactorialAsync");            
+            await Task.Run(() => Factorial());
+            //Console.WriteLine("Окончание работы метода FactorialAsync");
+        }
+    }
+}
